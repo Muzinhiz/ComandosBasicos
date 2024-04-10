3775105aa459ff8df73e5b836b9f06ccddbf81c0
Muzinhiz <gui_newyork@hotmail.com>
2024-04-10 17:04:26 -0300

    codigo de comandos basicos

---
+++ src/test/java/Calculadora/Calculo.java
@@ -0,0 +1,21 @@
+package Calculadora;
+
+public class Calculo {
+	
+	
+	
+    //class de execucao
+	public static void main(String[] args) {
+		
+		Operacoes operacoes = new Operacoes();
+		
+		operacoes.soma(50, 38);
+	
+		operacoes.subtracao(10, 5);	
+
+		operacoes.divisao(50, 2);
+		
+		operacoes.multiplicacao(40, 2);
+	}
+
+}
---
+++ src/test/java/Calculadora/Operacoes.java
@@ -0,0 +1,28 @@
+package Calculadora;
+//page
+public class Operacoes {
+    
+	public void soma(int x1, int x2) {
+		
+	int resultado = x1+x2;
+	System.out.println("O resultado da soma é " +resultado);}
+	
+	public void subtracao(int x1, int x2) {
+		int resultado = x1-x2;
+		System.out.println("O resultado da subtracao é " +resultado);}
+
+		public void divisao(int x1, int x2) {
+			int resultado = x1/x2;
+			System.out.println("O resultado da divisao é " + resultado);}
+		
+		public void multiplicacao (int x1, int x2) {
+			int resultado = x1*x2;
+			System.out.println("O resultado da multiplicacao é " +resultado);}
+	
+		
+	}
+	
+
+	
+
+
